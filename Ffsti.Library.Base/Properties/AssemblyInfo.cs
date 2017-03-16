80b90996deab05dc450516c8b3dce6d073a9af07
Carlos Forti <carlos.forti@fortieforti.com.br>
2017-03-15 23:30:46 -0300

    Versionamento

--- Ffsti.Library.Base/Properties/AssemblyInfo.cs
+++ Ffsti.Library.Base/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ffsti.Library.Base")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Biblioteca base para desenvolvimento")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Forti & Forti Soluções em TI")]
 [assembly: AssemblyProduct("Ffsti.Library.Base")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Forti & Forti Soluções em TI 2017")]
+[assembly: AssemblyTrademark("Forti & Forti Soluções em TI")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Ffsti.Library.Cep/Properties/AssemblyInfo.cs
+++ Ffsti.Library.Cep/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ffsti.Library.Cep")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Biblioteca para Consulta de CEP utilizando site dos Correios")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Forti & Forti Soluções em TI")]
 [assembly: AssemblyProduct("Ffsti.Library.Cep")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Forti & Forti Soluções em TI 2017")]
+[assembly: AssemblyTrademark("Forti & Forti Soluções em TI")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
